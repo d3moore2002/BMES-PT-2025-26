2d8285f01b77923297f0cc68f0a53de940af5f39
kguano13 <keguan@ucsd.edu>
2026-02-23 20:06:05 -0800

    Update Pressure_Sensor_&_Vibrator_Code.ino
    New changes: When pressure drops to 0 the vibration immediately stops (removed lingering vibration)

--- Software/Pressure_Sensor_&_Vibrator_Code.ino
+++ Software/Pressure_Sensor_&_Vibrator_Code.ino
@@ -5,6 +5,9 @@
 
 Adafruit_DRV2605 drv;
 
+unsigned long lastTime = 0;
+const unsigned long interval = 70; // adjust between 50-80ms to taste
+
 void setup() {
   Serial.begin(9600);
   Serial.println("DRV2605 Pressure Feedback Initializing...");
@@ -15,34 +18,32 @@ void setup() {
   }
 
   drv.selectLibrary(1);
-  // Internal trigger mode allows us to fire effects via the 'go' command
   drv.setMode(DRV2605_MODE_INTTRIG); 
 }
 
 void loop() {
-  // 1. Read the pressure sensor (Value between 0 and 1023)
-  int analogReading = analogRead(PRESSURE_SENSOR_PIN);
+  if (millis() - lastTime >= interval) {
+    lastTime = millis();
 
-  // 2. Map the pressure to an effect ID
-  // We map 0-1023 to 0-117. 
-  // Effect 0 is "Stop", 1-117 are various intensities.
-  uint8_t effect = map(analogReading, 0, 1023, 0, 117);
+    int analogReading = analogRead(PRESSURE_SENSOR_PIN);
 
-  // 3. Provide Serial Feedback
-  Serial.print("Pressure: ");
-  Serial.print(analogReading);
-  Serial.print(" -> Triggering Effect #");
-  Serial.println(effect);
+    int constrainedReading = constrain(analogReading, 0, 820);
+    uint8_t effect = map(constrainedReading, 0, 820, 0, 117);
 
-  // 4. Trigger the vibration if there is enough pressure
-  // We add a small "deadzone" (threshold of 20) so it doesn't vibrate constantly
+    Serial.print("Pressure: ");
+    Serial.print(analogReading);
+    Serial.print(" -> Triggering Effect #");
+    Serial.println(effect);
+
   if (analogReading > 20) {
-    drv.setWaveform(0, effect);  // Play the mapped effect
-    drv.setWaveform(1, 0);       // End waveform signifier
-    drv.go();                    // Play it!
+      drv.setWaveform(0, effect);
+      drv.setWaveform(1, effect);
+      drv.setWaveform(2, effect);
+      drv.setWaveform(3, effect);
+      drv.setWaveform(4, 0);
+      drv.go();
+    } else {
+      drv.stop(); // immediately halt motor when pressure is released
+    }
   }
-
-  // 5. Short delay to prevent overwhelming the I2C bus 
-  // and to let the haptic motor finish its cycle
-  delay(200); 
 }
